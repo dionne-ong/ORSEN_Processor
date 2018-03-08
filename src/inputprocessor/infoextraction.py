a19f229579091762a04a499807003d5fc8525bb2
katherine-07 <luisa_gilig@dlsu.edu.ph>
2018-03-08 14:58:31 +0800

    Pre-processing

--- src/inputprocessor/infoextraction.py
+++ src/inputprocessor/infoextraction.py
@@ -1,19 +1,56 @@
-import nltk
-
+import spacy
+from stanfordcorenlp import StanfordCoreNLP
 # ----- luisa
 
-def reading():
-    with open('document.txt', 'r') as f:
+
+def reading(filename):
+    with open(filename, 'r') as f:
         userinput = f.read()
+    return userinput
 
 
-def ie_processing():
-    sentences = nltk.sent_tokenize(userinput)
-    token_sentences = [nltk.word_tokenize(sent) for sent in sentences]
-    tagged_sentences = [nltk.pos_tag(sent) for sent in sentences]
-    chunked_sentences = nltk.ne_chunk(tagged_sentences)
+nlp = spacy.load('en')
+document = nlp(reading("document.txt"))
 
+sentences = [sent.string.strip() for sent in document.sents]
 
-#  ----- rachel
+for sent in sentences:
+    print(sent)
+    sent = nlp(sent)
+    for token in sent:
+        print("---POS----");
+        print(token.text, token.lemma_, token.pos_, token.tag_, token.dep_)
+        #displacy.serve(sent, style='dep')
+    for ent in sent.ents:
+        print("---NER----");
+        print(ent.text, ent.start_char, ent.end_char, ent.label_)
+        #displacy.serve(sent, style='ent')
+    for chunk in sent.noun_chunks:
+        print("----NC---");
+        print(chunk.text, chunk.root.text, chunk.root.dep_,
+              chunk.root.head.text)
 
+nlp = StanfordCoreNLP(r'C:\stanford-corenlp-full-2018-01-31', memory='8g')
+props = {'annotators': 'dcoref', 'pipelineLanguage': 'en', 'outputFormat': 'json'}
+output = [nlp.annotate(sent, properties=props) for sent in sentences]
+print(output)
 
+#ie_categorizing
+count = len(sentences)
+print("HAYYYYY")
+commands = []
+story_text = []
+for x in range(0, count):
+    check = sentences[x]
+
+    if 'orsen' not in check:
+        story_text.append(sentences[x])
+    else:
+        commands.append(sentences[x])
+
+#ie_semantic_role_labelling
+srl_entity_list = {"person", "place", "object"}
+result_entity = "Unknown"
+
+for entity in srl_entity_list:
+        result_entity = entity;
