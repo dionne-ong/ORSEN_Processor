4fc3192ce403b9b1dffb891877c9bb08505e2383
katherine-07 <luisa_gilig@dlsu.edu.ph>
2018-05-15 15:07:11 +0800

    detects attribute duplicates

--- src/inputprocessor/infoextraction.py
+++ src/inputprocessor/infoextraction.py
@@ -295,8 +295,12 @@ def add_capability(sent, attr, subject, world, num):
             for c in list_of_char:
                 c = str(c).lower()
                 if c in world.characters:
+                    new_attribute = check_duplicate_attribute(world.characters[c].attributes, new_attribute)
+                    if new_attribute is not None:
                         world.characters[c].attributes.append(new_attribute)
                 elif c in world.objects:
+                    new_attribute = check_duplicate_attribute(world.objects[c].attributes, new_attribute)
+                    if new_attribute is not None:
                         world.objects[c].attributes.append(new_attribute)
 
 
@@ -384,6 +388,15 @@ def add_objects(sent, child, dep, lemma, world, subject=""):
             add_attributes(sent, child, subject, world, "", DBO_Concept.HAS)
 
 
+def check_duplicate_attribute(obj_attributes, attribute):
+    for i in obj_attributes:
+        if i.name == attribute.name:
+            if i.isNegated is not attribute.isNegated:
+                i.isNegated = attribute.isNegated
+            return None
+    return attribute
+
+
 def add_attributes(sent, child, subject, world, negation="", relation=""):
     list_of_attributes = [child.lower()]
     list_of_char = char_conj_extractions(sent, subject)
@@ -408,11 +421,14 @@ def add_attributes(sent, child, subject, world, negation="", relation=""):
                 new_attribute = Attribute(relation, attr, negation)
                 char = world.characters[c]
                 print("ADD", attr, "TO", c)
-                char.attributes.append(new_attribute)
 
-                if relation == DBO_Concept.IS_A:
-                    print("RELATION", relation)
-                    char.type.append(attr)
+                new_attribute = check_duplicate_attribute(char.attributes, new_attribute)
+                if new_attribute is not None:
+                    char.attributes.append(new_attribute)
+
+                    if not relation == DBO_Concept.IS_A:
+                        print("RELATION", relation)
+                        char.type.append(attr)
 
         elif c in world.objects:
             for attr in list_of_attributes:
@@ -420,11 +436,15 @@ def add_attributes(sent, child, subject, world, negation="", relation=""):
                 new_attribute = Attribute(relation, attr, negation)
                 print("ADD", attr, "TO", c)
                 obj = world.objects[c]
-                obj.attributes.append(new_attribute)
 
-                if relation == DBO_Concept.IS_A:
-                    print("RELATION", relation)
-                    obj.type.append(attr)
+                new_attribute = check_duplicate_attribute(obj.attributes, new_attribute)
+                if new_attribute is not None:
+                    print(" ---------------------- ADDED IT ------------------------")
+                    obj.attributes.append(new_attribute)
+
+                    if relation == DBO_Concept.IS_A:
+                        print("RELATION", relation)
+                        obj.type.append(attr)
 
 
 def add_settings(sent, num, subject, negation, world):
