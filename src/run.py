0a4a29cd3ed68ef6946d1b28dbbc1bc3c2917000
Dionne Ong <dionne_ong@dlsu.edu.ph>
2018-06-30 00:36:17 +0800

    Log edits

--- src/run.py
+++ src/run.py
@@ -17,17 +17,14 @@ def new_world(id):
 
 def extract_info(text):
     world = server.get_world(world_id)
-    print(world)
     document = nlp(str(text))
     sentences = [sent.string.strip() for sent in document.sents]
     list_of_sentences = []
     list_of_sent = []
-    # Character
     characters = []
 
     # Part-Of-Speech, NER, Dependency Parsing
     for sent in sentences:
-        print(sent)
         sent = nlp(sent)
         list_of_sentences.append(infoextraction.pos_ner_nc_processing(sent))
 
@@ -40,21 +37,16 @@ def extract_info(text):
     for sent in list_of_sentences:
         if curr == 1 or curr > 1:
             sentences[curr] = infoextraction.coref_resolution(sent, sentences[curr], sentences[bef], world, False)
-            print("returned: ", sentences[curr])
         else:
             sentences[curr] = infoextraction.coref_resolution(sent, sentences[curr], sentences[curr], world, True)
-            print("returned: ", sentences[curr])
 
-        print("current index: ", curr)
         curr += 1
         if bef == 0 and curr == 1:
             print("oops")
         else:
             bef += 1
 
-    print(sentences)
     for s in sentences:
-        print(s)
         s = nlp(s)
         list_of_sent.append(infoextraction.pos_ner_nc_processing(s))
 
@@ -62,19 +54,19 @@ def extract_info(text):
         infoextraction.details_extraction(s, world, "ROOT")
         infoextraction.event_extraction(s, world, "ROOT")
 
-    print(world.characters)
-    print(world.objects)
-
+    print("-------- CHARACTERS")
     for c in world.characters:
         print(world.characters[c])
         for a in world.characters[c].attributes:
             print("attr", a.relation, a.name, a.isNegated)
 
+    print("-------- OBJECTS")
     for c in world.objects:
         print(world.objects[c])
         for a in world.objects[c].attributes:
             print("attr", a.relation, a.name, a.isNegated)
 
+    print("-------- SETTINGS")
     for s in world.settings:
         print(world.settings[s])
 
